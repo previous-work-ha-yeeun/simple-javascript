3157d6fd236090e923cdbf2591844d27db99701b
YeEun-Ha <hye8299@gmail.com>
2026-08-09 01:33:39 +0900

    Add user input fields

--- 02_conditional_decitions/scripts/script.js
+++ 02_conditional_decitions/scripts/script.js
@@ -7,13 +7,3 @@ const minimumAgeToAttend        = 21;
 /* tax rate is 10%, defined here as 0.10 */
 const taxRate                   = 0.10;
 
-/*
-these data points represent information about the current user
-NOTE: since this data would in theory be supplied by the user, all values 
-(even numeric) here are strings... 
-- change these values as you test your script!
-*/
-const userName              = "Shania Smith";
-const age                   = "21";
-const cashOnHandDollarsCDN  = "320";
-const quantityOfTickets     = "3";
