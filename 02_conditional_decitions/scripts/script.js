6431f11628ad50c8553cf8b1cacaa00cfcd3cc40
YeEun-Ha <hye8299@gmail.com>
2026-08-09 02:25:36 +0900

    Add user input validation and handling

--- 02_conditional_decitions/scripts/script.js
+++ 02_conditional_decitions/scripts/script.js
@@ -7,3 +7,19 @@ const minimumAgeToAttend        = 21;
 /* tax rate is 10%, defined here as 0.10 */
 const taxRate                   = 0.10;
 
+let inputName, inputAge, inputTickets, inputCash;
+
+function saveValue() {
+    inputName = document.getElementById("userName").value;
+    inputAge = Number(document.getElementById("userAge").value);
+    inputTickets = Number(document.getElementById("ticketQuantity").value);
+    inputCash = Number(document.getElementById("cashOnHand").value);
+    // console.log(inputName, inputAge, inputTickets, inputCash)
+}
+
+inputAge = Math.min(inputAge, 130);     // returns the smallest value
+inputTickets = Math.min(inputTickets, 100);     
+inputCash = Math.min(inputCash, 10000);     
+
+
+
