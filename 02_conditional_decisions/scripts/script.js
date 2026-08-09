860846261a31a9f70185093b71b4690278156c07
YeEun-Ha <hye8299@gmail.com>
2026-08-09 19:00:35 +0900

    Fix user input error on initial page load

--- 02_conditional_decisions/scripts/script.js
+++ 02_conditional_decisions/scripts/script.js
@@ -8,18 +8,38 @@ const minimumAgeToAttend        = 21;
 const taxRate                   = 0.10;
 
 let inputName, inputAge, inputTickets, inputCash;
+let processAge = "";
 
+
+// Initial page load 
+document.getElementById("javascript_message").innerHTML = "YeEun Ha";
+saveValue()
+
+
+// First Box
 function saveValue() {
     inputName = document.getElementById("userName").value;
     inputAge = Number(document.getElementById("userAge").value);
+    inputAge = Math.min(inputAge, 130);     // returns the smallest value
+
     inputTickets = Number(document.getElementById("ticketQuantity").value);
+    inputTickets = Math.min(inputTickets, 100);     
+    
     inputCash = Number(document.getElementById("cashOnHand").value);
-    // console.log(inputName, inputAge, inputTickets, inputCash)
+    inputCash = Math.min(inputCash, 10000);    
+
+    Introduce(inputName)
+    console.log(inputName, inputAge, inputTickets, inputCash)
 }
 
-inputAge = Math.min(inputAge, 130);     // returns the smallest value
-inputTickets = Math.min(inputTickets, 100);     
-inputCash = Math.min(inputCash, 10000);     
+function Introduce(name) {
+    document.getElementById("nameOfUser").innerHTML = `${name}`;
+    document.getElementById("userSays").innerHTML = `${name} says: "It's my first time to go to a concert in Vancouver!"`;
+}
+
+
+
+
 
 
 
