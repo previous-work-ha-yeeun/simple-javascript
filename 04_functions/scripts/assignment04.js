a7f6d41078cd7ca77bbe27e5e947b786fbc98230
YeEun-Ha <hye8299@gmail.com>
2026-08-22 17:01:33 +0900

    Render time information

--- 04_functions/scripts/assignment04.js
+++ 04_functions/scripts/assignment04.js
@@ -1,3 +1,22 @@
+/* This page invokes functions defined externally */
+
+const $time = document.getElementById('time')
+
+const dateObject = new Date();
+let currentHour = dateObject.getHours();
+
 
 document.getElementById("javascript_message").innerHTML = "YeEun Ha";
+
+/* --- Render the time info --- */
+$time.innerHTML = (currentHour <= 12) ?
+    `Good morning, the current hour is ${currentHour}, and the sun is shining. 
+    <img src="./images/sun.png" alt="sun" class="floatToRight" width="100px"></img>` 
+    : `Good evening, the current hour is ${currentHour}, and the moon is full. 
+    <img src="./images/moon.png" alt="mun" class="floatToRight" width="100px"></img>`;
+    
+
+console.log("-----------Part One----------")
+
+
 
